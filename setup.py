a558aecd23520b483f1f96bc9d6c178ea2ef832f
ivande <ivan@1fit.app>
2024-05-23 10:08:41 +0500

    cicd pandas

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ivande_combiner",
-    version="0.0.12",
+    version="0.0.13",
     packages=find_packages(),
     description="basic functionality for classic sklearn ml",
     long_description=open("README.md").read(),

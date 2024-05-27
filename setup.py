3b452cb03a8113414ded906fc6621c90fc712822
ivande <ivan@1fit.app>
2024-05-27 13:35:05 +0500

    max impute

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ivande_combiner",
-    version="0.0.17",
+    version="0.0.18",
     packages=find_packages(),
     description="basic functionality for classic sklearn ml",
     long_description=open("README.md").read(),

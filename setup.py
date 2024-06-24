c0a361e78a38cfa2c6afc22f56d8d92b9181c855
ivande <ivan@1fit.app>
2024-06-24 18:13:01 +0500

    expand_df

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ivande_combiner",
-    version="0.0.23",
+    version="0.0.24",
     packages=find_packages(),
     description="basic functionality for classic sklearn ml",
     long_description=open("README.md").read(),

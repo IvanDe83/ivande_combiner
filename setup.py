32f7eec7a24796ef89852de251201c4df4212f5c
ivande <ivan@1fit.app>
2024-10-24 10:41:15 +0500

    30

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ivande_combiner",
-    version="0.0.29",
+    version="0.0.30",
     packages=find_packages(),
     description="basic functionality for classic sklearn ml",
     long_description=open("README.md").read(),

2d927e453b01eec42b242bc535d3b8e343cec0d6
ivande <ivan@1fit.app>
2025-05-14 15:40:20 +0500

    bump version to 0.0.41 and update scikit-learn dependency to 1.6.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ivande_combiner",
-    version="0.0.40",
+    version="0.0.41",
     packages=find_packages(),
     description="basic functionality for classic sklearn ml",
     long_description=open("README.md").read(),
@@ -12,7 +12,7 @@ setup(
     url="https://github.com/IvanDe83/ivande_combiner",
     license="MIT",
     install_requires=[
-        "scikit-learn~=1.5.2",
+        "scikit-learn~=1.6.1",
         "pandas~=2.2.3",
         "requests~=2.32.3",
         "sqlalchemy~=2.0.36",
